36dcc19162593d4967bb524d18eaa115ed733979
Alberto Jaramillo <jose_a_1993@hotmail.com>
2024-06-13 12:03:06 -0600

    Add git issue notes

--- build/js/script.js
+++ build/js/script.js
@@ -5,6 +5,7 @@ $(function () {
   let meetMonsters = document.querySelector('#meet')
   let monsterScroll = document.querySelectorAll('#monster-group .monster')
   let navHeight = nav.scrollHeight
+  //TODO Here is missing somethign
 
   monsterScroll.forEach(
     (item) => (item.style.animationDelay = `${Math.random() * 1 + 0.4}s`)
